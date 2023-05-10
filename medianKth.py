f86fcb6ee8189421a9792e0362896bacdb13e28c
thatfoogabriel <sotogabriel1228@gmail.com>
2023-05-10 16:49:13 -0700

    implemented and tested all algorithms

--- medianKth.py
+++ medianKth.py
@@ -1,8 +1,57 @@
+import numpy as np
 import time
 
+def medianKth(arr, k):
+    if k > 0 and k <= len(arr):
+        left = 0
+        right = len(arr) - 1
+        pivotIndex = None
+        while pivotIndex != k-1:
+            pivotIndex, arr = partition(arr, left, right)
+            if pivotIndex < k-1:
+                left = pivotIndex+1
+            elif pivotIndex > k-1:
+                right = pivotIndex-1
+        return arr[pivotIndex]
+    else:
+        return None
 
+def partition(arr, left, right):
+    medians = []
+    for i in range(left, right+1, 5):
+        split = arr[i:i+5]
+        split.sort()
+        median = split[len(split)//2]
+        medians.append(median)
+    
+    pivot = arr[right]
+    i = left-1
+    for j in range(left, right):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    
+    arr[i+1], arr[right] = arr[right], arr[i+1]
+    return i+1, arr
 
-start = time.time()
-
-end = time.time()
-print("\nExecuted in ", (end-start) * 10**3, "ms")
+def main():
+    manual = input("Use custom array? (y/n) ")
+    if (manual == "y"):
+        print("Enter array entries (left to right, separated by space): ")
+        entries = list(map(int, input().split()))
+        arr = np.array(entries)
+        k = int(input("Enter value k for kth smallest value to look for: "))
+    else:
+        n = int(input("Enter array length: "))
+        arr = np.random.randint(0, 100, n)
+        k = int(input("Enter value k for kth smallest value to look for: "))
+    
+    print("Array: ", arr)
+    start = time.perf_counter()
+    kthSmallest = medianKth(arr.tolist(), k)
+    end = time.perf_counter()
+    print("Element Found: ", kthSmallest)
+    print("Executed in ", (end-start) * 10**6, "μs")
+    
+if __name__ == "__main__":
+    main()
--- mergeKth.py
+++ mergeKth.py
@@ -1,8 +1,58 @@
+import numpy as np
 import time
 
+def mergeKth(arr, k):
+    if k > 0 and k <= len(arr):
+        arr = mergeSort(arr)
+        return arr[k-1]
+    else:
+        return None
 
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = mergeSort(arr[mid:])
+        right = mergeSort(arr[:mid])
+        return merge(left, right)
+    else:
+        return arr
 
-start = time.time()
+def merge(left, right):
+    result = []
+    i = 0
+    j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i+=1
+        else:
+            result.append(right[j])
+            j+=1
+    if i < len(left):
+        result += left[i:]
+    elif j < len(right):
+        result += right[j:]
+    
+    return result
 
-end = time.time()
-print("\nExecuted in ", (end-start) * 10**3, "ms")
+def main():
+    manual = input("Use custom array? (y/n) ")
+    if (manual == "y"):
+        print("Enter array entries (left to right, separated by space): ")
+        entries = list(map(int, input().split()))
+        arr = np.array(entries)
+        k = int(input("Enter value k for kth smallest value to look for: "))
+    else:
+        n = int(input("Enter array length: "))
+        arr = np.random.randint(0, 100, n)
+        k = int(input("Enter value k for kth smallest value to look for: "))
+    
+    print("Array: ", arr)
+    start = time.perf_counter()
+    kthSmallest = mergeKth(arr.tolist(), k)
+    end = time.perf_counter()
+    print("Element Found: ", kthSmallest)
+    print("Executed in ", (end-start) * 10**6, "μs")
+    
+if __name__ == "__main__":
+    main()
